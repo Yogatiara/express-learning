630f80b57662447d9a89cb079220754a9b27a271
yoga <11211086@student.itk.ac.id>
2023-08-28 10:13:03 +0800

    connect to database

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const db = require('./connection')
+const response = require('./response')
+
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  console.log({ urlParam: req.query.alamat })
-  res.send('Hello World udin!')
+  // console.log({ urlParam: req.query.alamat })
+  const sql = "SELECT * FROM mahasiswa";
+  db.query(sql, (error, result) => {
+    // console.log(result);
+    response(200, result, "get all data from mahasiswa", res)
+  });
+
+
 })
 
-app.get('/yoga', (req, res) => {
-  res.send("Hello yoga")
-})
+app.get('/find', (req, res) => {
+  const sql = `SELECT * FROM mahasiswa WHERE nim = ${req.query.nim}`
+  db.query(sql, (error, result) => {
+    response(200, result, "find mahasiswa name", res)
 
+  })
+
+})
 
 // valiation
 app.post('/login', (req, res) => {
